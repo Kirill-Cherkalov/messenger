ee325252a7a98e2905ef08bac1513b879b7a6e5f
Konstantin Tatarchuk <konstantin.tatarchuk@effective-soft.com>
2019-09-20 13:33:08 +0300

    fix package.json migrations scripts && rm knex config duplication && fix created_at, updated_at, deleted_at users columns

--- knex/knexfile.migrate.ts
+++ knex/knexfile.migrate.ts
@@ -1,29 +1,4 @@
+require('ts-node/register')
 require('dotenv').config({ path: '../.env' });
 
-const shouldLogDebug = process.env.USE_KNEX_DEBUG && JSON.parse(process.env.USE_KNEX_DEBUG);
-
-const config = {
-  client: 'mysql',
-  useNullAsDefault: true,
-  debug: shouldLogDebug,
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    typeCast(field, next) {
-      if (field.type === 'TINY' && field.length === 1) {
-        const value = field.string();
-        return value ? value === '1' : null;
-      }
-      return next();
-    },
-  },
-  pool: {
-    min: 2,
-    max: 10,
-  },
-};
-
-module.exports = config;
+module.exports = require('./knexfile.ts').default;
